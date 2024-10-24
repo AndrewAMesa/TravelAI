3a2669aadabcac7aecdcb3b5ab5a4c9710bc7250
Andy <aamesa03@gmail.com>
2024-10-24 10:25:19 -0400

    fixing ui buttons

--- Frontend/src/Components/ActionButton.js
+++ Frontend/src/Components/ActionButton.js
@@ -1,18 +1,23 @@
-import React from 'react';
+const ActionButton = ({ label, icon, bgColor, onClick, isSmall }) => {
+  const [firstWord, secondWord] = label.split(" ");
 
-const ActionButton = ({ label, icon, bgColor, onClick }) => {
-    const [firstWord, secondWord] = label.split(" ");
-
-    return (
-        <div onClick={onClick} className={`h-48 w-2/4 flex flex-col items-center justify-center p-4 ${bgColor} text-[#563635] border border-[#563635] border-2 rounded-md hover:opacity-90 cursor-pointer`}>
-            <img src={icon} alt={label} className="h-8 w-8 mb-auto ml-auto" />
-            <div className="w-full text-left">
-                <p className="font-medium whitespace-pre-line">
-                    {firstWord} <span className="font-bold">{secondWord}</span>
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      onClick={onClick}
+      className={`transition-all duration-300 ease-in-out
+        ${isSmall ? 'w-32 h-32' : 'w-48 h-48'}
+        flex flex-col items-center justify-center mx-4
+        ${bgColor} text-[#563635] border border-[#563635] rounded-md 
+        hover:opacity-90 cursor-pointer`}
+    >
+      <img src={icon} alt={label} className="h-8 w-8 mb-2" />
+      <div className="w-full text-center">
+        <p className="font-medium whitespace-pre-line">
+          {firstWord} <span className="font-bold">{secondWord}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ActionButton;
